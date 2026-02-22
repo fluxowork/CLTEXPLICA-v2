07211b541911738ec7d696aa6217b14d9a1ec8a9
fluxowork <joaoandraderamon@gmail.com>
2026-02-22 12:33:15 -0300

    Add files via upload

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,1 +1,5 @@
-
+const SUPABASE_URL = 'https://hwhroyriokxluoyowcvx.supabase.com'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3aHJveXJpb2t4bHVveW93Y3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MTYzNzMsImV4cCI6MjA4NzA5MjM3M30.SxCAkKWWHUyDgqPTmf49cc28HlSbvqOQDSE9x5o7Z70'
+const { createClient } = supabase
+const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+window.sb = sb
